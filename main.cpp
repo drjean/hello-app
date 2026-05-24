bd2070d8e48ff06bfbe78aaa038892c11f9f44fd
Eugene <khanoh@gmail.com>
2026-05-24 12:09:02 -0500

    Test

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 int main() {
     std::cout << "Hello, C++ in VS Code!\n";
-    std::cout << "Hello, C++ in VS Code -2!\n";
+    std::cout << "Hello, C++ in VS Code - 2!\n";
+    std::cout << "Hello, C++ in VS Code - 3!\n";
     return 0; 
 }
